bfd6e4fc0689210e20a51dbd5cdb4763e3da30d3
Mahmuda Liza <mkl75@drexel.edu>
2016-02-20 18:38:05 -0500

    w/2 url - mahmuda

--- www/views/login/login.js
+++ www/views/login/login.js
@@ -3,7 +3,10 @@ angular.module('LRNMe',[])
 
 .controller('LoginCtrl', function($scope) {
   $scope.testVar = "I am Test";
-  //thois is ajs comment
-  // ajs second comment
-  // comment - mahmuda
+
+  $scope.registerProcess = function(){
+    // ..
+  }
+
+
 })
