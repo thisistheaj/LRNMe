b205aa7ede94f0b8e307a4cdfdb3963992cc178f
Anthony Beckner <ajbeckner@nc-114-183.dhcp.drexel.edu>
2016-02-20 18:57:50 -0500

    added popup and list

--- www/views/subgoals/subgoals.js
+++ www/views/subgoals/subgoals.js
@@ -1,5 +1,40 @@
 angular.module('LRNMe')
 
-  .controller('SubGoalsCtrl', function($scope) {
-    $scope.test = "test";
+  .controller('SubGoalsCtrl', function($scope,$ionicPopup) {
+    $scope.items = [
+      "one",
+      "two",
+      "three"
+    ]
+
+    $scope.addItem = function(item) {
+      items.push(item);
+
+    }
+
+    $scope.showPopup = function() {
+      $scope.data = {}
+
+      // Custom popup
+      $ionicPopup.show({
+        template: '<input type = "text" ng-model = "data.model">',
+        title: 'Add Item',
+        subTitle: 'What do you need to do?',
+        scope: $scope,
+
+        buttons: [
+          { text: 'Cancel' }, {
+            text: '<b>Add</b>',
+            type: 'button-positive',
+            onTap: function() {
+              if (!$scope.data.model) {
+                e.preventDefault();
+              } else {
+                $scope.addItem($scope.data.model);
+              }
+            }
+          }
+        ]
+      });
+    };
   })
